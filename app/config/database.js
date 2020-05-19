f29536e51a16d6b08a807c72579a5045f6b4cb6e
ednilson <ednilson@medlynx.com.br>
2020-05-19 13:54:02 -0300

    Removendo linha comentada

--- app/config/database.js
+++ app/config/database.js
@@ -8,5 +8,4 @@ module.exports = {
         timestamps: true,
         underscored: true,
     }
-    // dialect: 'mysql', host: 'localhost', username: 'root', password: '32312095', database: 'staging_area', define: { timestamps: true, underscored: true, }
 }
