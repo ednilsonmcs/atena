473513ceca4c0f90674e705b33a52c9c8065ab33
ednilson <ednilson@medlynx.com.br>
2020-05-19 10:45:25 -0300

    Configurando MySQL com Google Cloud

--- app/config/database.js
+++ app/config/database.js
@@ -1,11 +1,12 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
+    host: '35.199.97.0',
     username: 'root',
-    password: '32312095',
+    password: 'partenon',
     database: 'staging_area',
     define: {
         timestamps: true,
         underscored: true,
     }
+    // dialect: 'mysql', host: 'localhost', username: 'root', password: '32312095', database: 'staging_area', define: { timestamps: true, underscored: true, }
 }
