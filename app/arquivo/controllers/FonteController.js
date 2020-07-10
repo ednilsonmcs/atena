bd1bc30566340f43e41a9c6ef842766b5dee42c6
ednilson <ednilson@medlynx.com.br>
2020-07-09 21:48:43 -0300

    featureListarFOnte - Realizar a carga do arquivo

--- app/arquivo/controllers/FonteController.js
+++ app/arquivo/controllers/FonteController.js
@@ -1,30 +1,78 @@
 const Fonte = require('../models/Fonte');
-const { carregarWorkbook } = require('../../commom/funcoes');
+const { carregarWorksheet, getFile } = require('../../commom/funcoes');
 const fs = require('fs');
 const { PATH_FONTE } = require('../../config/env.config');
 
 module.exports = {
     async store(req,res){
+        const hash = req.body.hash;
+        let fonte;
         let quantidade_registros = 0;
-        let ws = null;
-        await carregarWorkbook(path)
-        .then( workbook => {
-            ws = workbook.getWorksheet("Plan1");
-            quantidade_registros = ws.rowCount;     
-        }).catch( err => { 
-          console.error( err );
-        });
+        const andressOfFile = await getFile(PATH_FONTE,hash);
 
-        const fonte = await Fonte.create({
-            nome:'nome', 
-            hash:'hash',
-            quantidade_registros,
-            usuario:'',
-            carregado: false
-        });
-    return res.json(fonte);
+        if(andressOfFile){
+            await carregarWorksheet(andressOfFile)
+                .then(async (worksheet) => {
+                    quantidade_registros = worksheet.rowCount;
+        
+                    fonte = await Fonte.create({
+                        nome:'nome', 
+                        hash:'hash',
+                        quantidade_registros,
+                        usuario:'',
+                        carregado: false
+                    });
+                    worksheet.eachRow({ includeEmpty: true }, function(row, rowNumber) {
+        
+                    data = row.getCell(1).value;
+                    data += " "+row.getCell(3).value;
+                    dia_semana = row.getCell(2).value;
+                    endereco = row.getCell(4).value;
+                    bairro = row.getCell(5).value;
+                    municipio = row.getCell(6).value;
+                    cod_tipo = row.getCell(7).value;
+                    descricao = row.getCell(8).value;
+                    cod_sub_tipo = row.getCell(9).value;
+                    desc_sub_tipo = row.getCell(10).value;
+                    sit_encontrada = row.getCell(11).value;
+                    descricao_finalizacao = row.getCell(12).value;
+                    historico = row.getCell(13).value;
+        
+                    const itenfonte = {
+                        data,
+                        dia_semana,
+                        endereco,
+                        bairro,
+                        municipio,
+                        cod_tipo,
+                        descricao,
+                        cod_sub_tipo,
+                        desc_sub_tipo,
+                        sit_encontrada,
+                        descricao_finalizacao,
+                        historico
+                    }
+                
+                    });
+                    
+                    fonte = await Fonte.create({
+                        nome:'nome', 
+                        hash:'hash',
+                        quantidade_registros,
+                        usuario:'',
+                        carregado: false
+                    });
+                })
+
+            return res.status(200).json(fonte);
+        }else{
+            return res.status(404).json({erro: 'Arquivo inexistente!'});
+        }
+
+
+
     },
-    listar(req,res){
+    async listar(req,res){
         let listFiles = [];
         fs.readdir(PATH_FONTE, (err, files) => {
             files.forEach(name => {
@@ -33,6 +81,21 @@ module.exports = {
                 hash = metaFile[0];
                 type = metaType[1];
                 listFiles.push({ hash, name, type })
+            });
+            return res.json(listFiles);
+        });
+    },
+    async listarByHash(req,res){
+        let listFiles = [];
+        fs.readdir(PATH_FONTE, (err, files) => {
+            files.forEach(name => {
+                let metaFile = name.split('-');
+                let metaType = name.split('.');
+                hash = metaFile[0];
+                type = metaType[1];
+                if(req.params.hash == hash){
+                    listFiles.push({ hash, name, type })
+                }
             });
             return res.json(listFiles);
         });
--- app/config/database.js
+++ app/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     dialect: 'mysql',
-    host: '35.199.97.0',
+    host: 'localhost',
     username: 'root',
-    password: 'partenon',
+    password: '32312095',
     database: 'staging_area',
     timezone: '-03:00',
     define: {
@@ -10,3 +10,17 @@ module.exports = {
         underscored: true,
     }
 }
+
+// Gloogle Cloud
+// module.exports = {
+//     dialect: 'mysql',
+//     host: '35.199.97.0',
+//     username: 'root',
+//     password: 'partenon',
+//     database: 'staging_area',
+//     timezone: '-03:00',
+//     define: {
+//         timestamps: true,
+//         underscored: true,
+//     }
+// }
